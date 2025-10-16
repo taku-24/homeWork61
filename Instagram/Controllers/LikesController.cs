4068d5edef80bbcc137500eeaebcd058018bf628
Taiyrbek Anarbekov <an.taku2455@gmail.com>
2025-10-16 14:55:27 +0600

    update action and update name of variables

--- Instagram/Controllers/LikesController.cs
+++ Instagram/Controllers/LikesController.cs
@@ -6,35 +6,44 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Instagram.Controllers;
 
+
 [Authorize]
 public class LikesController : Controller
 {
-    private readonly InstagramContext _ctx;
-    private readonly UserManager<User> _um;
-    public LikesController(InstagramContext ctx, UserManager<User> um) { _ctx = ctx; _um = um; }
+    private readonly InstagramContext _context;
+    private readonly UserManager<User> _userManager;
+    public LikesController(InstagramContext context, UserManager<User> userManager)
+    {
+        _context = context;
+        _userManager = userManager;
+    }
 
     [HttpPost]
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> Toggle(int postId)
     {
-        var me = await _um.GetUserAsync(User);
-        var post = await _ctx.Posts.FindAsync(postId);
-        if (post == null) return NotFound();
+        var me = await _userManager.GetUserAsync(User);
+        var post = await _context.Posts.FindAsync(postId);
+        if (post == null)
+            return Json(new { success = false, message = "Пост не найден" });
 
-        var like = await _ctx.Likes.FirstOrDefaultAsync(l => l.PostId == postId && l.UserId == me.Id);
+        var like = await _context.Likes.FirstOrDefaultAsync(l => l.PostId == postId && l.UserId == me.Id);
+        bool liked;
+
         if (like == null)
         {
-            _ctx.Likes.Add(new Like { PostId = postId, UserId = me.Id });
+            _context.Likes.Add(new Like { PostId = postId, UserId = me.Id });
             post.LikesCount++;
+            liked = true;
         }
         else
         {
-            _ctx.Likes.Remove(like);
+            _context.Likes.Remove(like);
             post.LikesCount = Math.Max(0, post.LikesCount - 1);
+            liked = false;
         }
-        await _ctx.SaveChangesAsync();
 
-        return RedirectToAction("Index", "Home");
+        await _context.SaveChangesAsync();
+        return Json(new { success = true, liked, likesCount = post.LikesCount });
     }
 }
-
